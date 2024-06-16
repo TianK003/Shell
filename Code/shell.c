52b7627ccda8c78f5832e8a789390a68456ae313
TianK003 <tian.kljucanin@gmail.com>
2024-06-16 16:21:22 +0200

    Shell now suppports debugging for easier development and testing

--- Code/shell.c
+++ Code/shell.c
@@ -1,46 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_INPUT 512
 #define MAX_TOKENS 64
+#define NO_INTERNAL_COMMANDS 5
 // ta tabela ima vhodno vrstico
 char line[MAX_INPUT];
+char zaPrint[MAX_INPUT];
 // v to tabelo si hranim samo indekse, kjer se zacnejo besede
 int tokens[MAX_TOKENS];
 // si belezim koliko tokenov sem nasel
 int tokenCount = 0;
-char shellName[9];
-int native = 0;
+// spremlja debug nivo
+int debugLevel = 0;
+int izpisiDebugLevel = 0;
 
-// funkcija pripravi vrstico za izhodno obliko
+// ce se izvaja v ospredju je 0, ce v ozadju je 1
+int background = 0;
+char shellName[9];
+int zadnjiStatus = 0;
+// da ves ce je ukaz zunanji
+int isExternal = 0;
+// da ves kako printat
+int jePrimerenDebug = 1;
+char *interniUkazi[] = {"debug", "exit", "help", "prompt", "status"};
+int previousDebugLevel = 0;
+// da ves ce interaktivno izvajas
+int native = 0;
+// posebej za debuganje, da se izpise vse kot mora
+int spremeniNazajNaNic = 0;
+
+// izpise vhod, tokene in redirecte
+void printToken() {
+    int j = 0;
+    for (int i = 0; i < tokenCount; i++) {
+            printf("Token %d: '%s'\n", j, line + tokens[i]);
+            j++;
+            if (i == 0 && debugLevel > 0 && strcmp(line + tokens[i], "debug") == 0 && jePrimerenDebug == 1) i++;
+    }
+}
+
 void printInputLine() {
-    int r, n = 0;
-    for (int i = 0; i < MAX_INPUT; i++) {
-        // carriage return smo ze popravili
-        if (r && n) break;
-        // popravi po potrebi
-        if (line[i] == '\n') {
-            line[i] = '\0';
-            n = 1;
+    if (debugLevel > 0) {
+        printf("Input line: '%s'\n", zaPrint);
+        printToken();
+    }    
+}
+
+void redirect() {
+    int count = tokenCount;
+    //printf("tokencount je %d\n", tokenCount);
+    for (int i = 0; i < count; i++) {
+        char beseda = line[tokens[i]];
+        if (beseda == '>') {
+            tokenCount--;
+            if (debugLevel > 0) printf("Output redirect: '%s'\n", line + tokens[i] + 1);
         }
-        if (line[i] == '\r') {
-            line[i] = '\0';
-            r = 1;
+        else if (beseda == '<') {
+            tokenCount--;
+            if (debugLevel > 0) printf("Input redirect: '%s'\n", line + tokens[i] + 1);
+        }
+        else if (beseda == '&') {
+            tokenCount--;
+            background = 1;
+            if (debugLevel > 0) printf("Background: '%d'\n", background);
         }
     }
-    // izpisi input line
-    printf("Input line: '%s'\n", line);
+    //printf("tokencount je %d\n", tokenCount);
+    
+    // posebej obravnavam ce je zunanji ukaz
+    if (isExternal) {
+        printf("External command '");
+        for (int i = 0; i < tokenCount; i++) {
+            if (i != tokenCount-1) {
+                printf("%s ", line + tokens[i]);
+            }
+            else {
+                printf("%s", line + tokens[i]);
+            }
+        }
+        printf("'\n");
+        return;
+    }
+    else return;
+}
+
+void pripraviInput() {
+    for (int i = 0; i < MAX_INPUT; i++) {
+        if (line[i] == '\n') line[i] = '\0';
+        if (line[i] == '\r') line[i] = '\0';
+        zaPrint[i] = line[i];
+    }
 }
 
 // najde indekse tokenov v vhodu
 void tokenize() {
-    // najdi vse tokene v input line
     int isciPrvega = 1;
     int jeVNarekovajih = 0;
-
     for (int i = 0; i < MAX_INPUT; i++) {
         // naleteli smo na komentar, lahko zakljucimo s to vrstico
         if (line[i] == '#' && jeVNarekovajih == 0 && isciPrvega == 1) {
@@ -83,11 +142,136 @@ void tokenize() {
             tokenCount++;
         }
     }
+}
 
-    // izpisi tokene
-    for (int i = 0; i < tokenCount; i++) {
-        printf("Token %d: '%s'\n", i, line + tokens[i]);
+void debug() {
+    previousDebugLevel = debugLevel;
+    // samo izpises trenutni debug level
+    if (tokenCount == 1) {
+        printInputLine();
+        redirect();
+        izpisiDebugLevel = 1;
+    } 
+    // spreminjas ali pa tudi ne
+    else {
+        debugLevel = atoi(line + tokens[1]);
+        if (debugLevel == 0 && line[tokens[1]] != '0') {
+            jePrimerenDebug = 0;
+            // zacasno spremeni debugLevel na 1, da izpise debugLevel
+            debugLevel = 1;
+            printInputLine();
+            redirect();
+            spremeniNazajNaNic = 1;
+        }
+        else {
+            jePrimerenDebug = 1;
+        }
+        izpisiDebugLevel = 0;
     }
+    zadnjiStatus = 0;
+}
+
+void izhod() {
+    if (tokenCount == 2) {
+        zadnjiStatus = atoi(line + tokens[1]);
+    }
+    exit(zadnjiStatus);
+}
+
+void help() {
+    printf("Izpis vseh moznih ukazov in njihova uporaba:\n");
+    printf("- \033[0;32mdebug [level]\033[0m nastavi debug level na [level] ali izpise trenutno vrednost ce argument ni podan.\n");
+    printf("- \033[0;32mexit [status]\033[0m zapre shell z statusom [status] ali z zadnjim zabelezenim statusom, ce argument ni podan.\n");
+    printf("- \033[0;32mhelp\033[0m izpise vse moznosti ukazov in njihovo uporabo.\n");
+    printf("- \033[0;32mprompt [name]\033[0m nastavi ime shell-a na [name] ali izpise trenutno ime ce argument ni podan.\n");
+    printf("- \033[0;32mstatus\033[0m izpise se izhodni status zadnjega izvedenega ukaza. Status pusti nespremenjen.\n");
+    printf("\n\033[0;33mOpozorilo\033[0m Ostali ukazi so zunanji in se niso implementirani. Pocakati boste morali na naslednjo nadgradnjo te naloge.\n");
+    zadnjiStatus = 0;
+}
+
+void prompt() {
+    if (tokenCount == 1) printf("%s\n", shellName);
+    else {
+        if (strlen(line + tokens[1]) > 8) {
+            zadnjiStatus = 1;
+            return;
+        }
+        else {
+            strcpy(shellName, line + tokens[1]);
+            // printf("%s\n", shellName);
+        }
+    }
+    zadnjiStatus = 0;
+}
+
+void status() {
+    printf("%d\n", zadnjiStatus);
+}
+
+void execute_builtin(int ukaz) {
+    if (line[tokens[tokenCount]] == '&') {
+        background = 1;
+    }
+    else background = 0;
+
+    switch (ukaz) {
+        case 0:
+            // posebna funkcija
+            debug();
+            break;
+        case 1:
+            izhod();
+            printInputLine();
+            redirect();
+            break;
+        case 2:
+            help();
+            printInputLine();
+            redirect();
+            break;
+        case 3:
+            prompt();
+            printInputLine();
+            redirect();
+            break;
+        case 4:
+            status();
+            printInputLine();
+            redirect();
+            break;
+        default:
+            printf("Napaka pri prepoznavanju internega ukaza! Do sem ne bi smel priti.\n");
+            break;
+    }
+}
+
+int find_builtin() {
+    for (int i = 0; i < NO_INTERNAL_COMMANDS; i++) {
+        if (strcmp(line + tokens[0], interniUkazi[i]) == 0) {
+            isExternal = 0;
+            execute_builtin(i);
+            // mal grdo hendlanje debugga, ampak deluje
+            if (debugLevel > 0 && previousDebugLevel > 0) {
+                printf("Executing builtin '%s' in %s\n", interniUkazi[i], background ? "background" : "foreground" );
+                if (spremeniNazajNaNic) {
+                    debugLevel = 0;
+                    spremeniNazajNaNic = 0;
+                }
+            }
+            // izpisuje debug level
+            if (i == 0 && izpisiDebugLevel) {
+                printf("%d\n", debugLevel);
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void execute_external() {
+    isExternal = 1;
+    printInputLine();
+    redirect();
 }
 
 int main(int argc, char *argv[]) {
@@ -96,11 +280,20 @@ int main(int argc, char *argv[]) {
     if (native) printf("%s> ", shellName);
 
     while (fgets(line, sizeof(line), stdin) != NULL) {
-        printInputLine();
+        pripraviInput();
         tokenize();
+        if (tokenCount == 0) {
+            if (native) { printf("%s> ", shellName); }
+            continue;
+        }
+        // poisci ukaz
+        if (find_builtin() == 0) {
+            execute_external();
+        }
         // sprazni input line, da ni problemov pri naslednjem branju
         for (int i = 0; i < MAX_INPUT; i++) {
             line[i] = '\0';
+            zaPrint[i] = '\0';
         }
         // sprazni se tokens tabelo
         for (int i = 0; i < MAX_TOKENS; i++) {
